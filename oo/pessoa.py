5d2a96f6251abcde79e5efa52f038d46061f3c78
BaldomeroPerez <mirogperez@hotmail.com>
2020-09-02 19:44:14 -0300

    criado atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):  # depois de virgula entrou none que estava em baixo
+        self.idade = idade
+        self.nome = nome  #nome entro no lugar de None
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ =='__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano') #luciano entrou no lugar  de None (em branco)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "Miro"
+    print(p.nome)
+    print(p.idade)
