c4c870048de906268bef7cdd18e9fd6247fc454c
BaldomeroPerez <mirogperez@hotmail.com>
2020-09-05 22:14:13 -0300

    criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):  # depois de virgula entrou none que estava em baixo
+    def __init__(self, *filhos, nome=None, idade=35):  # depois de virgula entrou none que estava em baixo
         self.idade = idade
         self.nome = nome  #nome entro no lugar de None
+        self.filhos=list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ =='__main__':
-    p = Pessoa('Luciano') #luciano entrou no lugar  de None (em branco)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Miro"
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo,nome='Luciano') #luciano entrou no lugar  de None (em branco)
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filhos in luciano.filhos:
+        print(filhos.nome)
+
